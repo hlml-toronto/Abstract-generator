4ff3684941d81583d0ff0ac91c6fc2e846a67daf
mattsmart <mattsmart@hotmail.ca>
2021-04-22 15:30:49 -0400

    raw arxiv dataset v1

--- arxiv_api.py
+++ arxiv_api.py
@@ -1,13 +1,16 @@
 import urllib.request
 import feedparser
 
+import csv
+
+
 # Base api query url
 base_url = 'http://export.arxiv.org/api/query?';
 
 # Search parameters
 search_query = 'all:electron' # search for electron in all fields
 start = 0                     # retreive the first 5 results
-max_results = 5
+max_results = 10
 
 query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
                                                      start,
@@ -18,8 +21,8 @@ query = 'search_query=%s&start=%i&max_results=%i' % (search_query,
 # Some entry metadata lives in the arXiv namespace.
 # This is a hack to expose both of these namespaces in
 # feedparser v4.1
-feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
-feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
+#feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+#feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
 
 # perform a GET request using the base_url and query
 response = urllib.request.urlopen(base_url+query).read()
@@ -40,8 +43,18 @@ abstract_list = []
 
 # Run through each entry, and print out information
 for entry in feed.entries:
-
-    abstract_list.append(entry.summary)
-
+    #print(entry.keys())
+    data_row = [
+        entry.id,
+        entry.arxiv_primary_category,
+        entry.tags,
+        entry.title,
+        entry.summary]
+    abstract_list.append(data_row)
 
 print(abstract_list)
+fields = ['id', 'arxiv_primary_category', 'tags', 'title', 'summary']
+with open('raw_arxiv_%d.csv' % max_results, mode='w') as csv_file:
+    write = csv.writer(csv_file)  
+    write.writerow(fields)
+    write.writerows(abstract_list)
