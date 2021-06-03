6bf6d49c8f66e23ef445db7843e2bda74931495c
Jeremy Rothschild <jerben.rothschild@gmail.com>
2021-06-03 14:51:35 -0400

    Dataset and Dataloader nearly completed, need to figure out padding map for src and tgt.

---
+++ bumbleBERT/src/data/custom_dataset.py
@@ -0,0 +1,58 @@
+from torch.utils.data import Dataset
+import pandas as pd
+
+class ArxivDataset(Dataset):
+    """
+    NOTE: Preprocessing can happen in here!
+
+    Describe what this Dataset does
+    """
+    def __init__(self, csvfile, maxLength=None, transform=None):
+        self.data = pd.read_csv(csvfile)
+        # last one r'\s+|\\n' seems to be the only one that works
+        remove_lst = ['\r\n','\n','\ n',r'\\n',r'\n',r'\s+|\\n']
+        self.data.replace(remove_lst,' ',regex=True, inplace=True)
+        self.transform = transform  # HuggingFace tokenizer
+        self.maxLen = maxLength
+        self.get_instance = self.get_instance_pretransform
+        if self.transform is not None:
+            self.get_instance = self.get_instance_transformed
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # uses function as defined by self.get_instance
+        return self.get_instance(idx)
+
+    def set_transform(self, transform):
+        # can set transform and will change how we get an instance
+        self.transform = transform
+        self.get_instance = self.get_instance_transformed
+
+    def get_instance_pretransform(self, idx):
+        # returns simply the text of summary
+        return self.data['summary'][idx]
+
+    def get_instance_transformed(self, idx):
+        # once tranform is defined, can get item already transformed
+        instance = self.get_instance_pretransform(idx)
+        # tokenize on-the-fly
+        instance = self.transform( instance
+                                    , max_length=self.maxLen+1
+                                    , padding='max_length'
+                                    , truncation=True
+                                    , return_tensors='pt'
+                                    )
+        # source and target are slightly shifted
+        return { 'src' : instance['input_ids'][0][:-1]
+                        , 'trg' : instance['input_ids'][0][1:] }
+
+    """
+    @staticmethod
+    def collate_fn(batches: List[Dict[str, int]]):
+        return {
+            k: torch.tensor(v, dtype=torch.int64)
+            for k, v in batches[0].items()
+        }
+    """
--- bumbleBERT/src/data/tokenization.py
+++ bumbleBERT/src/data/tokenization.py
@@ -1,55 +1,69 @@
 import os
+import numpy as np
+from pathlib import Path
 
-from tokenizers import Tokenizer, normalizers, pre_tokenizers, decoders, processors
+from tokenizers import Tokenizer, normalizers, pre_tokenizers, decoders\
+                                , processors
 from tokenizers.models import BPE, Unigram, WordLevel, WordPiece
 from tokenizers.normalizers import NFD, NFKD, NFC, NFKC, Lowercase, StripAccents
-from tokenizers.pre_tokenizers import ByteLevel, Whitespace, WhitespaceSplit, Punctuation, Metaspace,\
-                                        CharDelimiterSplit
-from tokenizers.trainers import BpeTrainer, UnigramTrainer, WordPieceTrainer, WordLevelTrainer
-from pathlib import Path
+from tokenizers.pre_tokenizers import ByteLevel, Whitespace, WhitespaceSplit\
+                                                , Punctuation, Metaspace\
+                                                , CharDelimiterSplit
+from tokenizers.trainers import BpeTrainer, UnigramTrainer, WordPieceTrainer\
+                                            , WordLevelTrainer
+from transformers import PreTrainedTokenizerFast
 
 #from transformers import PreTrainedTokenizerFast, PreTrainedTokenizer
 
-def train_custom_tokenizer(token_model, data_iterator, token_filename, token_dir, vocab_size=30000
-                                    , vocab=None, max_input_chars_per_word=None, eos_token=None
-                                    , bos_token=None, pad_token=None, mask_token=None
+def train_custom_tokenizer(token_model, dataset, token_filename, token_dir
+                                    , vocab_size, vocab=None
+                                    , max_input_chars_per_word=None
+                                    , eos_token=None, bos_token=None
+                                    , pad_token=None, mask_token=None
                                     , unk_token=None):
     """
     Building a Tokenizer using HuggingFace library. The pipeline seems to be:
 
-        - Model           : algorithm that tokenizes, it is a mandatory component. There are
-                            only 4 models implemented (BPE, Unigram, WordLevel, WordPiece)
-        - Normalizer      : some preprocessing that could happen before, but doesn't necessarily
+        - Model           : algorithm that tokenizes, it is a mandatory
+                            component. There are only 4 models implemented
+                            (BPE, Unigram, WordLevel, WordPiece)
+        - Normalizer      : some preprocessing that could happen before, but
+                            doesn't necessarily have to
         - Pre-Tokenizer   : splitting the input according to some rules
-        - Post-Processing : needing to add some tokens/input after (mostly seems to be eos
-                            , bos tokens)
-        - Decoder         : certain previous pipeline steps need to be reversed for proper
-                            decoding
+        - Post-Processing : needing to add some tokens/input after (mostly seems
+                            to be eos, bos tokens)
+        - Decoder         : certain previous pipeline steps need to be reversed
+                            for proper decoding
         - Trainer         : The corresponding training algorithm for the model
 
-    Note : Some pre-processing might need to happen beforehand in previous functions (might
-            be easier using pandas)
+    Note : Some pre-processing might need to happen beforehand in previous
+            functions (might be easier using pandas before)
 
     Input
         token_model              : algorithm to use for tokenization
-        data_iterator            : a python iterator that goes through the data to be used for
-                                    training
+        datasets                 : a python iterator that goes through the data
+                                    to be used for training
         token_dir                : directory with tokenizers
         vocab_size               : size of the vocabulary to use
-        token_filename           : filename of particular token we want to train. Will overwrite
-                                    previously save files.
-        vocab                    : models other than BPE can use non-mandatory vocab as input
+        token_filename           : filename of particular token we want to
+                                    train. Will overwrite previously save files.
+        vocab                    : models other than BPE can use non-mandatory
+                                    vocab as input
         max_input_chars_per_word : used for WordPiece
 
     Output
-        tokenizer                : huggingFace Tokenizer object, our fully trainer tokenizer
+        tokenizer                : huggingFace Tokenizer object, our fully
+                                    trainer tokenizer
 
     """
-    special_token_lst = [unk_token, bos_token, eos_token, pad_token, mask_token]
+    special_token_lst = [pad_token, bos_token, eos_token, mask_token, unk_token]
 
-    normalizer_lst = [NFKC()]; pre_tokenizer_lst = [ByteLevel()]; decoder_lst = []
+    #NFKC
+    normalizer_lst = []; pre_tokenizer_lst = [Whitespace, ByteLevel];
+    decoder_lst = []
 
-    bos_idx = special_token_lst.index(bos_token); eos_idx = special_token_lst.index(eos_token)
+    bos_idx = special_token_lst.index(bos_token);
+    eos_idx = special_token_lst.index(eos_token)
 
     if token_model == 'BPE':
         model   = BPE(unk_token=unk_token)
@@ -61,71 +75,76 @@ def train_custom_tokenizer(token_model, data_iterator, token_filename, token_dir
         model   = WordLevel(unk_token=unk_token,vocab=vocab)
         Trainer = WordLevelTrainer
     elif token_model == 'WordPiece':
-        model   = WordPiece(unk_token=unk_token,vocab=vocab, max_input_chars_per_word=max_input_chars_per_word)
+        model   = WordPiece(unk_token=unk_token,vocab=vocab
+                            , max_input_chars_per_word=max_input_chars_per_word)
         Trainer = WordPieceTrainer
     else:
-        error_msg = f'Error: token_model ({token_model}) not an algorithm in [BPE, Unigram, WordLevel, WordPiece]'
+        error_msg = f'Error: token_model ({token_model}) not an algorithm in\
+                        [BPE, Unigram, WordLevel, WordPiece]'
         raise SystemExit(error_msg)
 
     # instantiation
     tokenizer = Tokenizer(model)
 
     # trainer
-    trainer = Trainer(vocab_size=vocab_size, show_progress=True, special_tokens=special_token_lst)
+    if vocab_size == None:
+        trainer = Trainer(show_progress=True, special_tokens=special_token_lst)
+    else:
+        trainer = Trainer(vocab_size=vocab_size, show_progress=True
+                                            , special_tokens=special_token_lst)
 
     # normalizer
-    tokenizer.normalizer = normalizers.Sequence( normalizer_lst )
+    tokenizer.normalizer = normalizers.Sequence(
+                                    [fcn() for fcn in normalizer_lst] )
 
     # pre-tokenizer
-    tokenizer.pre_tokenizer = pre_tokenizers.Sequence( pre_tokenizer_lst )
+    tokenizer.pre_tokenizer = pre_tokenizers.Sequence(
+                                    [fcn() for fcn in pre_tokenizer_lst] )
 
     # post-processing
-    tokenizer.post_processor = processors.TemplateProcessing( single=bos_token+" $A "+eos_token
-                                                    #, pair=bos_token+" $A "+eos_token" $B:1 "+eos_token+":1"
-                                                    , special_tokens=[(bos_token, bos_idx),(eos_token, eos_idx)]
-                                                    )
+    tokenizer.post_processor = processors.TemplateProcessing(
+                    single=bos_token+" $A "+eos_token
+                    #, pair=bos_token+" $A "+eos_token" $B:1 "+eos_token+":1"
+                    , special_tokens=[(bos_token, bos_idx),(eos_token, eos_idx)]
+                    )
 
     # decoder
-    if ByteLevel() in pre_tokenizer_lst: tokenizer.decoder = decoders.ByteLevel()
-    if Metaspace() in pre_tokenizer_lst: tokenizer.decoder = decoders.Metaspace()
+    if ByteLevel in pre_tokenizer_lst:
+        tokenizer.decoder = decoders.ByteLevel()
+    if Metaspace in pre_tokenizer_lst: tokenizer.decoder = decoders.Metaspace()
     if token_model == 'WordPiece' : tokenizer.decoder = decoders.WordPiece()
 
-    tokenizer.train_from_iterator(trainer=trainer, iterator=data_iterator)
+    # creating iterator
+    def batch_iterator():
+        for i in np.arange(0,len(dataset)):
+            yield dataset[i]
 
+    # train call
+    tokenizer.train_from_iterator(trainer=trainer, iterator=batch_iterator()
+                                    , length=len(dataset))
+
+    # save file
     if not os.path.exists( token_dir ):
         os.makedirs( token_dir )
-    if os.path.exists( token_dir + os.sep + token_filename ):
-        print(f"Warning : overwriting previously save tokenizer with same filename ( {token_filename} ).")
-    tokenizer.save( token_dir + os.sep + token_filename + '_' + token_model + '.json' )
-
-    # TODO : Should I add PreTrained and Fast Tokenizer here? Seems like it might be appropriate.
-    transformer = False; fast = False
-    function_from_transformer_todo = None
-    if transformer:
-        raise SystemExit("HuggingFace transformers library not yet implemented here!")
-        if fast: tokenizer = function_from_transformer_todo
-        else: tokenizer = function_from_transformer_todo
+    tknzrfile = token_dir + os.sep + token_filename +'_'+ token_model + '.json'
+    if os.path.exists( tknzrfile ):
+        print(f"Warning : overwriting previously save tokenizer with\
+                        same filename ( {token_filename} ).")
+    tokenizer.save( tknzrfile )
 
     return tokenizer
 
 
-def load_custom_tokenizer(token_dir, token_filename, transformer=False, fast=False):
+def load_tokenizer(tknzrFile, eos_token=None, bos_token=None
+                            , pad_token=None, mask_token=None, unk_token=None):
     """
     Input
-        token_dir      : directory with tokenizers saved
-        token_filename : trained tokenizer that we want to load
-        transformer    : (bool) whether to use HuggingFace transformers library implementation
-        fast           : (bool) whether to use HuggingFace transformers fast implementation
+        tknzrFile : .json file of the tokenizer trained previously
     Output
-        tokenizer      : tokenizer from Tokenizer class to be passed to rest of algorithm
+        tknzr     : tokenizer as PreTrainedTokenizerFast class to be passed on
     """
-    tokenizer = Tokenizer.from_file(token_dir + os.sep + token_filename)
+    tknzr = PreTrainedTokenizerFast(tokenizer_file=tknzrFile)
+    tknzr.pad_token = pad_token
+    tknzr.mask_token = mask_token
 
-    function_from_transformer_todo = None
-    if function_from_transformer != None:
-        if transformer:
-            raise SystemExit("HuggingFace transformers library not yet implemented here!")
-            if fast: tokenizer = function_from_transformer_todo
-            else: tokenizer = function_from_transformer_todo
-
-    return tokenizer
+    return tknzr
