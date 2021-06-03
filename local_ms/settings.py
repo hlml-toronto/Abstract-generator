a5188bb4bb78577c4e173a6b46923ad50e5aedde
mattsmart <mattsmart@hotmail.ca>
2021-06-03 19:06:54 -0400

    bptt as default in settings.py

--- local_ms/settings.py
+++ local_ms/settings.py
@@ -1,4 +1,14 @@
+import os
+
 DIR_DATA = 'data'
 DIR_TOKENIZERS = 'tokenizers'
+DIR_MODELS = 'models'
 
 DEFAULT_DATASET = 'raw_arxiv_10.csv'
+
+# determines sequence length for get_batch(); used elsewhere e.g. train()
+BPTT = 35  # constant used by model.py (acts as a max token context length)
+
+for core_dir in [DIR_DATA, DIR_TOKENIZERS, DIR_MODELS]:
+    if not os.path.exists(core_dir):
+        os.mkdir(core_dir)
