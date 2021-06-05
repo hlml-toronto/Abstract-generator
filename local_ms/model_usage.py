a365a03fc41ed060b71473cffc448756777e16e5
mattsmart <mattsmart@hotmail.ca>
2021-06-05 00:10:48 -0400

    add probability visualization (of output)

--- local_ms/model_usage.py
+++ local_ms/model_usage.py
@@ -1,10 +1,13 @@
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
 from torchtext.datasets import WikiText2
 
 from model import TransformerModel, PositionalEncoding  # need all class definitions for un-pickle
-from model import load_model
-from model_utils import gen_tokenizer_and_vocab
+from model import load_model, evaluate
+from model_utils import gen_tokenizer_and_vocab, data_process, batchify
 from settings import DIR_MODELS, BPTT
 
 
@@ -19,10 +22,13 @@ def tokenize_some_text(text='The dog ran across the'):
     return tokenized_text, tokenized_text_ints
 
 
-def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', tokens_to_gen=10):
+def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', tokens_to_gen=10,
+                  vis=False):
     """
     dummy_token: if text_prompt is < BPTT tokens, need to add dummy tokens and mask them
     1) tokenize the text prompt
+
+    If vis: plot distribution over vocab for next word, given the past BPTT
     """
     total_text_string = text_prompt  # this will be extended by tokens_to_gen
 
@@ -43,10 +49,8 @@ def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', to
             input_slice = tokenized_text[0:nn]
             src_mask = model.generate_square_subsequent_mask(nn).to(device)
 
-        #processed_tokenized_text = torch.zeros(BPTT, dtype=torch.long) + dummy_token
-        #processed_tokenized_text[0:nn] = input_slice
-        src = torch.zeros((nn,1), dtype=torch.long)
-        src[0:nn, 0] = tokenized_text
+        src = torch.zeros((min(nn, BPTT), 1), dtype=torch.long)
+        src[0:nn, 0] = input_slice
         src.to(device)
 
         return text_split, src, src_mask
@@ -61,17 +65,41 @@ def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', to
     # 2)
     model.eval()
     for idx in range(tokens_to_gen):
+
+        running_context_string = ' '.join([vocab.itos[src[k]] for k in range(src.shape[0])])
+
         out = model.forward(src, src_mask)
         print(out.shape)
 
-        """
-        for idx in range(5):
-            print('Input %d:' % idx, prompt_split[0:idx+1])
-            qq = torch.argmax(out[idx,0,:])
-            print('Guess %d:' % idx, vocab.itos[qq])
-            print(qq)
-            print(vocab.itos[qq])
-        """
+        if vis:
+            next_word_weights = out[nn-1, 0].detach().numpy()
+            next_word_weights_exp = np.exp(next_word_weights)
+            next_word_probs = next_word_weights_exp / np.sum(next_word_weights_exp)
+
+            plt.plot(next_word_weights)
+            plt.title('next_word_weights: iteration %d' % idx)
+            plt.xlabel('vocab index')
+            plt.ylabel('weight')
+            #plt.xlim((-0.5,100.5))
+            plt.show()
+
+            plt.plot(next_word_probs)
+            plt.title('next_word_probs: iteration %d' % idx)
+            plt.xlabel('vocab index')
+            plt.ylabel('weight')
+            #plt.xlim((-0.5,100.5))
+            plt.show()
+
+            kk = 20
+            top_word_indices = np.argsort(next_word_probs)[::-1]
+            top_word_probs = next_word_probs[top_word_indices]
+            x = list(range(kk))
+            plt.title('next_word_probs: iteration %d' % idx)
+            plt.suptitle(running_context_string + ' ???', fontsize=8, wrap=True)
+            plt.xticks(x, [vocab.itos[k] for k in top_word_indices[0:kk]], rotation=60)
+            plt.plot(x, top_word_probs[0:kk])
+            plt.ylabel('probability')
+            plt.show()
 
         best_guess_int = torch.argmax(out[nn-1, 0])  # care batch dimension, nn vs BPTT
         best_guess_string = vocab.itos[best_guess_int]
@@ -103,8 +131,8 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # specify path to model or model_weights
-    model_path_A = DIR_MODELS + os.sep + 'model_epoch3.pth'
-    model_path_B = DIR_MODELS + os.sep + 'model_weights_epoch3.pth'
+    model_path_A = DIR_MODELS + os.sep + 'model_epoch100.pth'
+    model_path_B = DIR_MODELS + os.sep + 'model_weights_epoch100.pth'
 
     # load dataset, tokenizer, vocab
     tokenizer, vocab = gen_tokenizer_and_vocab()
@@ -121,9 +149,15 @@ if __name__ == '__main__':
     print('model_A == model_B:', model_A == model_B)
 
     # Text generation example
-    prompt = 'Text generation is easier than you think , however'
-    ngen = 30
-    generated_text = gen_some_text(model_B, vocab, device, text_prompt=prompt, tokens_to_gen=ngen)
+    #prompt = 'Text generation is easier than you think , however'
+    prompt = ' Text generation is easier than you think , however , but i can be a lot of the game . . . . . . . . . . . . . . . . the guitar hero , the game was released in the game was released in the game was released in the game , and the game , and the game , and the game . the game was released in the game was released in the game , and the game , and the game was released in the game . the game was released in the game , and the game , and the'
+    ngen = 100
+    generated_text = gen_some_text(
+        model_A, vocab, device, text_prompt=prompt, tokens_to_gen=ngen, vis=True)
     print("Text prompt:\n", prompt)
     print("Number of tokens to generate:", ngen)
     print("Generated_text:\n", generated_text)
+
+    # TODO: alternative generation
+    # currently 'greedy method'
+    # see: https://huggingface.co/blog/how-to-generate
