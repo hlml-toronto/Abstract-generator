5d2e2d8f786c1393159a717f29fd228c212863c5
mattsmart <mattsmart@hotmail.ca>
2021-06-16 22:36:00 -0400

    implement next word sampling

--- local_ms/model_usage.py
+++ local_ms/model_usage.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import log_softmax
 
 import torch
 from torchtext.datasets import WikiText2
@@ -23,13 +24,19 @@ def tokenize_some_text(text='The dog ran across the'):
 
 
 def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', tokens_to_gen=10,
-                  vis=False):
+                  vis=False, decode_style='greedy'):
     """
     dummy_token: if text_prompt is < BPTT tokens, need to add dummy tokens and mask them
     1) tokenize the text prompt
 
     If vis: plot distribution over vocab for next word, given the past BPTT
     """
+    # decoder hyperparameters
+    DECODE_SAMPLE_BETA = 1.2  # above 2 seems too high
+    DECODE_SAMPLE_SEED = 103
+    DECODE_SAMPLE_TOPP_THRESHOLD = 0.85
+    np.random.seed(DECODE_SAMPLE_SEED)
+
     total_text_string = text_prompt  # this will be extended by tokens_to_gen
 
     def process_prompt(dummy_token=0):
@@ -54,6 +61,70 @@ def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', to
         src.to(device)
 
         return text_split, src, src_mask
+
+    def decode(model_out, style='greedy'):
+        """
+        Sampling notes:
+        - working in log space to improve numeric stability
+        - use "Gumbel-max trick"
+          - https://stats.stackexchange.com/questions/64081/how-do-i-sample-from-a-discrete-categorical-distribution-in-log-space
+          - https://en.wikipedia.org/wiki/Categorical_distribution#Sampling_via_the_Gumbel_distribution
+        TODO - verify that this trick works the same as the classic way, for stability we need to compare with high noise (low beta) -- how to make the random samples identical?
+        """
+        assert style in ['greedy', 'sample_full', 'sample_topp']
+        if style == 'greedy':
+            guessed_int = torch.argmax(model_out[nn-1, 0])  # care batch dimension, nn vs BPTT
+        elif style == 'sample_full':
+            """
+            next_word_weights = out[nn-1, 0].detach().numpy()
+            next_word_weights_exp = np.exp(DECODE_SAMPLE_BETA * next_word_weights)
+            print('exp check')
+            print(next_word_weights_exp)
+            next_word_probs = next_word_weights_exp / np.sum(next_word_weights_exp)
+            print("LIMITS::::::::::::::::", np.min(next_word_probs), np.max(next_word_probs))
+            print("LIMITS::::::::::::::::", np.min(next_word_weights_exp), np.max(next_word_weights_exp))
+            print("LIMITS::::::::::::::::", np.sum(next_word_weights_exp))
+            unirand = np.random.rand()
+            distribution_cumsum = np.cumsum(next_word_probs)
+            guessed_int = np.searchsorted(distribution_cumsum, unirand)
+            print('BBBBBBBBBBBBBBBBBBBBBBBBBBB', type(unirand), unirand)
+            print(distribution_cumsum)
+            print(guessed_int)
+            print(distribution_cumsum[guessed_int-1], distribution_cumsum[guessed_int], distribution_cumsum[guessed_int+1])
+            print()"""
+
+            # numerically stable approach: gumbel max-trick sampling
+            next_word_weights = out[nn-1, 0].detach().numpy()
+            ncategories = next_word_weights.shape[0]
+            next_word_weights_scaled = DECODE_SAMPLE_BETA * next_word_weights
+            print("ncategories", ncategories)
+            uvec = np.random.rand(ncategories)
+            gvec = -np.log(-np.log(uvec))
+            guessed_int = np.argmax(gvec + next_word_weights_scaled)
+        else:
+            assert style == 'sample_topp'
+            # TODO implement gumbel max trick here too
+            next_word_weights = out[nn-1, 0].detach().numpy()
+            next_word_weights_exp = np.exp(DECODE_SAMPLE_BETA * next_word_weights)
+            next_word_probs = next_word_weights_exp / np.sum(next_word_weights_exp)
+            # 1) identify top p words such their cumulative probability passes threshold
+            distribution_sorted_indices = np.argsort(next_word_probs)[::-1]
+            next_word_probs_descsort = next_word_probs[distribution_sorted_indices]
+            next_word_probs_descsort_cumsum = np.cumsum(next_word_probs_descsort)
+            threshold_index = np.searchsorted(next_word_probs_descsort_cumsum,
+                                              DECODE_SAMPLE_TOPP_THRESHOLD)
+            # 2) sample from these top p words
+            topp_indices = distribution_sorted_indices[:threshold_index + 1]
+            print(distribution_sorted_indices)
+            print(next_word_probs_descsort_cumsum)
+            print("topp_indices", len(topp_indices), topp_indices)
+            topp_probs = next_word_probs_descsort[:threshold_index + 1]
+            topp_reweighted_probs = topp_probs / np.sum(topp_probs)
+            topp_reweighted_cumsum = np.cumsum(topp_reweighted_probs)
+            unirand = np.random.rand()
+            topp_choice = np.searchsorted(topp_reweighted_cumsum, unirand)
+            guessed_int = topp_indices[topp_choice]
+        return guessed_int
 
     # 1) tokenize the text prompt and prepare associated src_mask for model.forward()
     prompt_split, src, src_mask = process_prompt()  # src should be in form ntokens x nbatches
@@ -115,19 +186,19 @@ def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', to
             plt.ylabel('probability')
             plt.show()
 
-        best_guess_int = torch.argmax(out[nn-1, 0])  # care batch dimension, nn vs BPTT
-        best_guess_string = vocab.itos[best_guess_int]
-        print('best_guess_int, best_guess_string:', best_guess_int, best_guess_string)
+        next_guess_int = decode(out, style=decode_style)
+        next_guess_string = vocab.itos[next_guess_int]
+        print('next_guess_int, next_guess_string:', next_guess_int, next_guess_string)
 
         # update total_text_string by adding best guess
-        total_text_string += ' %s' % best_guess_string
+        total_text_string += ' %s' % next_guess_string
 
         # update src and src mask for next pass of model.forward()
         if nn < BPTT:
             # extend and shift the running window of input data
             src_updated = torch.zeros((nn + 1,1), dtype=torch.long)  # extend src to nn
             src_updated[0:nn, 0] = src[:, 0]
-            src_updated[nn, 0] = best_guess_int
+            src_updated[nn, 0] = next_guess_int
             src = src_updated.to(device)
             # increment mask dimension
             src_mask = model.generate_square_subsequent_mask(nn + 1).to(device)
@@ -135,7 +206,7 @@ def gen_some_text(model, vocab, device, text_prompt='The dog ran across the', to
         else:
             src_orig = src.clone()
             src[0:BPTT-1, 0] = src_orig[1:, 0]
-            src[-1, 0] = best_guess_int
+            src[-1, 0] = next_guess_int
 
     return total_text_string
 
@@ -145,8 +216,9 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # specify path to model or model_weights
-    model_path_A = DIR_MODELS + os.sep + 'model_epoch100.pth'
-    model_path_B = DIR_MODELS + os.sep + 'model_weights_epoch100.pth'
+    epoch = 100
+    model_path_A = DIR_MODELS + os.sep + 'model_epoch%d.pth' % epoch
+    model_path_B = DIR_MODELS + os.sep + 'model_weights_epoch%d.pth' % epoch
 
     # load dataset, tokenizer, vocab
     tokenizer, vocab = gen_tokenizer_and_vocab()
@@ -163,10 +235,13 @@ if __name__ == '__main__':
     print('model_A == model_B:', model_A == model_B)
 
     # Text generation example
-    prompt = 'Text generation is easier than you think , however'
-    ngen = 10
+    #prompt = 'Text generation is easier than you think , however'
+    prompt = 'The dog ran across the'
+    ngen = 60
+    decode_style = 'sample_full'
     generated_text = gen_some_text(
-        model_A, vocab, device, text_prompt=prompt, tokens_to_gen=ngen, vis=False)
+        model_A, vocab, device, text_prompt=prompt, tokens_to_gen=ngen, vis=False,
+        decode_style=decode_style)
     print("Text prompt:\n", prompt)
     print("Number of tokens to generate:", ngen)
     print("Generated_text:\n", generated_text)
